cd4cada215f636a7343c32cefa5fedf2ee91663a
FlaringPhoenix <joshmeubanks@gmail.com>
2021-10-04 18:26:44 -0500

    add: repo url

--- commands/credits.js
+++ commands/credits.js
@@ -10,6 +10,7 @@ module.exports = {
             embeds: [
                 new MessageEmbed()
                     .setTitle('Credits')
+                    .setDescription('XenoTools is opensource on [GitHub](https://github.com/FlaringPhoenix/XenoTools)!')
                     .addFields([
                         { name: 'FlaringPhoenix#0001', value: 'Founder, Head Developer\n[GitHub](https://github.com/FlaringPhoenix) - [Twitter](https://twitter.com/phoenix_flaring)' },
                         { name: 'physxx#4326', value: 'Network Engineer\n[Twitch](https://www.twitch.tv/physx_x_x) - [Twitter](https://twitter.com/Xx_physx_xX)'}
