00824382fae5a381d0996d580946095e93590b5c
FlaringPhoenix <joshmeubanks@gmail.com>
2021-10-04 22:25:06 -0500

    fix: discord status

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,9 @@
 module.exports = async (client) => {
-    client.user.setActivity(`${client.guilds.cache.size} guilds | ${client.commands.size} commands`, { type: 'WATCHING' });
+    client.user.setActivity('just started!', { type: 'PLAYING' });
+
+    setTimeout(function() {
+        client.user.setActivity(`${client.guilds.cache.size} guilds`, { type: 'WATCHING' });
+    }, 5000);
+
     global.logger.info('XenoTools is online!');
 }
